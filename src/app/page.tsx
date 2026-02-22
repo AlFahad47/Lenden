ea9f86d56bc3ac198e9bb6d36bfc928f862bc5a4
Rong <rongrubaiya@gmail.com>
2026-02-22 12:27:03 +0600

    Homepage Hero section for user done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Banner from "@/components/ui/Banner";
 import AboutPage from "./about/page";
+import BannerUser from "@/components/ui/BannerUser";
 
 export default function Home() {
   return (
     <main>
       <Banner />
+      <BannerUser/>
     </main>
   );
 }
