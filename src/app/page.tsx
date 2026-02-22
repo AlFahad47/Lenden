8362c4edb04c8ec84af56ca8e2ce38d490df111c
Rong <rongrubaiya@gmail.com>
2026-02-22 15:04:36 +0600

    Updated the logo for light theme support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,12 @@
 import Banner from "@/components/ui/Banner";
-<<<<<<< HEAD
-import HowItWorksPage from "@/components/ui/HowItWorks";
-=======
-import AboutPage from "./about/page";
+
 import BannerUser from "@/components/ui/BannerUser";
 
 export default function Home() {
   return (
     <main>
       <Banner />
-<<<<<<< HEAD
-      <HowItWorksPage />
-=======
-      <BannerUser/>
->>>>>>> 2af8cf30f58022c0621a4f065fbbc62f4c8b2616
+      {/* <BannerUser/> */}
     </main>
   );
 }
