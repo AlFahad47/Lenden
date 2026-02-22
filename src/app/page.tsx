e543f2bd844f00ef1133f0f2224d4bbc8ee41705
kohinurfatema <kohinurfatema35@gmail.com>
2026-02-22 01:46:46 -0800

    add: import OfferSection in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Banner from "@/components/ui/Banner";
 import AboutPage from "./about/page";
+import OfferSection from "@/components/ui/OfferSection";
 
 export default function Home() {
   return (
     <main>
       <Banner />
+      <OfferSection />
+
     </main>
   );
 }
