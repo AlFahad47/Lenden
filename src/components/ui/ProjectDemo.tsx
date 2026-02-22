c954dbc40deb0c670f2fef80a832294fa59f78f4
kohinurfatema <kohinurfatema35@gmail.com>
2026-02-22 03:22:27 -0800

    add: rename BalanceTracker to ProjectDemo component

---
+++ src/components/ui/ProjectDemo.tsx
@@ -0,0 +1,11 @@
+ "use client";
+
+  export default function ProjectDemo() {
+    return (
+      <section className="w-full py-16 px-6 bg-white dark:bg-gray-950 transition-colors duration-300">
+        <div className="max-w-6xl mx-auto">
+          vedio is comming
+        </div>
+      </section>
+    );
+  }
