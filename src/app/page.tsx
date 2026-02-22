9aff17614d9643534581345c181318ce2af8c742
kohinurfatema <kohinurfatema35@gmail.com>
2026-02-22 03:29:18 -0800

    add: import ProjectDemo section in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Banner from "@/components/ui/Banner";
 import AboutPage from "./about/page";
 import OfferSection from "@/components/ui/OfferSection";
+import ProjectDemo from "@/components/ui/ProjectDemo";
 
 export default function Home() {
   return (
     <main>
       <Banner />
       <OfferSection />
+       <ProjectDemo />
 
     </main>
   );
