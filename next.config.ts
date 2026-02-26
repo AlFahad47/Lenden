9e87b9a6adfad99e977f24c216e3c0c2f9e4e54b
MD Fahad Molla <ahfahad47sss@gmail.com>
2026-02-26 11:13:47 +0600

    typescript bug

--- next.config.ts
+++ next.config.ts
@@ -9,11 +9,6 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
