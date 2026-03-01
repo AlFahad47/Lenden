f2b2b1ef2e3fe2432acb3b78032f07af793bbcb2
JarifTheKing <jarifanuwar7.com>
2026-03-01 23:29:52 +0600

    icon changed

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -15,16 +15,22 @@ import {
   ChevronLeft,
   ChevronRight,
   UserCog,
+  ShieldCheckIcon,
+  FileCheck,
 } from "lucide-react";
 
 const sidebarItems = [
   { icon: LayoutDashboard, label: "Dashboard", path: "/dashboard" },
   { icon: UserCog, label: "Admin", path: "/dashboard/admin" },
-  { icon: Users, label: "User Request", path: "/dashboard/userRequest" },
+  {
+    icon: ShieldCheckIcon,
+    label: "User Request",
+    path: "/dashboard/userRequest",
+  },
   { icon: Users, label: "Users", path: "/dashboard/users" },
   { icon: CreditCard, label: "Transactions", path: "/dashboard/transactions" },
   { icon: BarChart3, label: "Analytics", path: "/dashboard/analytics" },
-  { icon: ShieldCheck, label: "KYC", path: "/dashboard/kyc" },
+  { icon: FileCheck, label: "KYC", path: "/dashboard/kyc" },
   { icon: Settings, label: "Settings", path: "/dashboard/settings" },
 ];
 
