8527c21c4a9ec8240a96b19e1e52d0b9f244f50e
kohinurfatema <kohinurfatema35@gmail.com>
2026-03-01 02:16:21 -0800

    feat: integrate BannerSwitch into homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,36 @@
-import Banner from "@/components/ui/Banner";
-import OfferSection from "@/components/ui/OfferSection";
-import ProjectDemo from "@/components/ui/ProjectDemo";
-import HowItWorksPage from "@/components/ui/HowItWorks";
-import KeyFeatures from "@/components/ui/KeyFeatures";
-import Menus from "@/components/ui/menus";
-import BannerUser from "@/components/ui/BannerUser";
+ import BannerSwitch from "@/components/ui/BannerSwitch";
+  import OfferSection from "@/components/ui/OfferSection";
+  import ProjectDemo from "@/components/ui/ProjectDemo";
+  import HowItWorksPage from "@/components/ui/HowItWorks";
+  import KeyFeatures from "@/components/ui/KeyFeatures";
+  import Menus from "@/components/ui/menus";
 
-export default function Home() {
-  return (
-   <main>
-      <section id="home">
-        <Banner />
-      </section>
+  export default function Home() {
+    return (
+      <main>
+        <section id="home">
+          <BannerSwitch />
+        </section>
 
-      <section>
-        <BannerUser></BannerUser>
-      </section>
+        <section id="menus">
+          <Menus />
+        </section>
 
-      <section id="menus">
-        <Menus />
-      </section>
+        <section id="offers">
+          <OfferSection />
+        </section>
 
-      <section id="offers">
-        <OfferSection />
-      </section>
+        <section id="features">
+          <KeyFeatures />
+        </section>
 
-      <section id="features">
-        <KeyFeatures />
-      </section>
+        <section id="how">
+          <HowItWorksPage />
+        </section>
 
-      <section id="how">
-        <HowItWorksPage />
-      </section>
-
-      <section id="demo">
-        <ProjectDemo />
-      </section>
-    </main>
-  );
-}
+        <section id="demo">
+          <ProjectDemo />
+        </section>
+      </main>
+    );
+  }
